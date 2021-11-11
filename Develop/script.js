29f92c7a0b550f3de16a276f92ed4280fdc1e9fc
Jonathan Chen <jontkc98@gmail.com>
2021-11-11 13:03:23 -0800

    Added function that retrieves the length of the password from the user.

--- Develop/script.js
+++ Develop/script.js
@@ -5,29 +5,51 @@ var generateBtn = document.querySelector("#generate");
 var lowerGroup = "abcdefghijklmnopqrstuvwxyz";
 var upperGroup = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numsGroup = "1234567890";
-var specialGroup = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~"; //I'm missing quotation i don't wanna deal with escape characters
+var specialGroup = "\" !#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 
 
 
-// Write password to the #password input
-function writePassword() {
+// Add event listener to generate button
+generateBtn.addEventListener("click", start);
 
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+function start() {
+  // ask user how long they want their password to be
+  let passLen = passwordLength();
+  // check boxes asking what characters they want their password to have
+  
+  // run generate password
 
-  passwordText.value = password;
+  // display the password
+
+}
+
+function passwordLength() {
+  var passLen = Number(prompt("How long do you want your password to be? (8-128 char limit)", 12));
+  while (!Number.isInteger(passLen) || passLen < 8 || passLen > 128) {
+    if (!Number.isInteger(passLen)) {
+      // console.log(typeof passLen);
+      // console.log(passLen);
+      passLen = Number(prompt("This is not a number. Try again (8-128 char limit)", 12));
+    } else if (passLen < 8) {
+      // console.log(typeof passLen);
+      // console.log(passLen);
+      passLen = Number(prompt("Minimum length is 8 characters. Try again (8-128 char limit)", 12));
+    } else {
+      // console.log(typeof passLen);
+      // console.log(passLen);
+      passLen = Number(prompt("Maximum length is 128 characters. Try again (8-128 char limit)", 12));
+    }
+  }
+  return passLen;
 }
 
 
-
 function generatePassword() {
-  let passLen = how long the password needs to be;
-
   let charGroup = "";
-  let lower = boolean lower;
-  let upper = boolean upper;
-  let nums= boolean numbers;
-  let special = boolean special;
+  // let lower = boolean lower;
+  // let upper = boolean upper;
+  // let nums= boolean numbers;
+  // let special = boolean special;
 
   if (lower) {
     charGroup = charGroup + lowerGroup;
@@ -47,18 +69,13 @@ function generatePassword() {
 
 
   let answer = "";
-  while (answer.length < how long the password needs to be) {
-    let temp = charGroup.charAt(Math.floor(Math.random() * charGroup.length));
-    answer = answer + temp;
-  }
+  // while (answer.length < how long the password needs to be) {
+  //   let temp = charGroup.charAt(Math.floor(Math.random() * charGroup.length));
+  //   answer = answer + temp;
+  // }
 
   return answer;
 }
-
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
 
 
 /* <div class="card-options">
@@ -69,3 +86,13 @@ generateBtn.addEventListener("click", writePassword);
   <output name="x" for="a b"></output>
 </form>
 </div> */
+
+
+
+function writePassword() {
+
+  // var password = generatePassword();
+  // var passwordText = document.querySelector("#password");
+
+  // passwordText.value = password;
+}
